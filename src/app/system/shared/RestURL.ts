6cc74867b3039d1a4e1a431068be6e45ed8f376e
MOBITEL\NAVEENDRAK <naveendrak@mobitel.lk>
2024-04-29 03:07:40 +0530

    Add URLs

---
+++ src/app/system/shared/RestURL.ts
@@ -0,0 +1,8 @@
+const mainURL = 'http://localhost:8080/';
+
+export const RestURL = {
+  getAllTasks           : mainURL + 'api/tasks',
+  addTask               : mainURL + 'api/tasks/add',
+  updateTaskById        : mainURL + 'update',
+  deleteTaskById        : mainURL + 'delete'
+}
